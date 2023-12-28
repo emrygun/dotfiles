3ee019f2b405fe6365654216fa2ac3ef68406cab
Emre Uygun <emre6271@gmail.com>
2023-12-28 19:20:05 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# dotfiles
+Dotfiles that I'm currently using
+
+* i3/config
+* i3status/config
+* init.lua
+* .bashrc
+
+Emre Uygun – [@emrygun](https://twitter.com/emrygun) – contact@emreuygun.dev
---
+++ init.lua
@@ -0,0 +1,470 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+----------------
+--- plugins ---
+----------------
+require("lazy").setup({
+
+  -- colorscheme
+  { 
+    "sainnhe/sonokai", 
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function ()
+      vim.cmd([[colorscheme sonokai]])
+    end,
+  },
+
+  -- statusline
+  { 
+    "nvim-lualine/lualine.nvim",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+      require("lualine").setup({
+        options = { theme = 'gruvbox' },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+          }
+        }
+      })
+    end,
+  },
+
+  -- search selection via *
+  { 'bronson/vim-visual-star-search' },
+
+  -- testing framework
+  { 
+    "vim-test/vim-test",
+    config = function ()
+      vim.g['test#strategy'] = 'neovim'
+      vim.g['test#neovim#start_normal'] = '1'
+      vim.g['test#neovim#term_position'] = 'vert'
+    end,
+  },
+
+  {
+    'dinhhuy258/git.nvim',
+    config = function ()
+      require("git").setup()
+    end,
+  },
+
+
+  -- file explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        filters = {
+          dotfiles = true,
+        },
+        on_attach = function(bufnr)
+          local api = require('nvim-tree.api')
+
+          local function opts(desc)
+            return {
+              desc = 'nvim-tree: ' .. desc,
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+              nowait = true,
+            }
+          end
+
+          api.config.mappings.default_on_attach(bufnr)
+
+          vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
+          vim.keymap.set('n', 'i', api.node.open.horizontal, opts('Open: Horizontal Split'))
+          vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
+        end
+      })
+    end,
+  },
+
+  -- save my last cursor position
+  {
+    "ethanholz/nvim-lastplace",
+    config = function()
+      require("nvim-lastplace").setup({
+        lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+        lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+        lastplace_open_folds = true
+      })
+    end,
+  },
+
+  -- markdown
+  {
+    "iamcco/markdown-preview.nvim",
+    dependencies = {
+      "zhaozg/vim-diagram",
+      "aklt/plantuml-syntax",
+    },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    ft = "markdown",
+    cmd = { "MarkdownPreview" },
+  },
+
+  -- commenting out lines
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require('Comment').setup({
+        opleader = {
+          ---Block-comment keymap
+          block = '<Nop>',
+        },
+      }) 
+    end
+  },
+
+  { 
+    "AndrewRadev/splitjoin.vim"
+  },
+
+  -- fzf extension for telescope with better speed
+  {'nvim-telescope/telescope-ui-select.nvim' },
+
+  -- fuzzy finder framework
+  {
+    "nvim-telescope/telescope.nvim", 
+    tag = '0.1.4',
+    dependencies = { 
+      "nvim-lua/plenary.nvim" ,
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function ()
+      require("telescope").setup({
+        extensions = {
+          fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                             -- the default case_mode is "smart_case"
+          }
+        }
+      })
+
+    end,
+  },
+
+  { 
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ":TSUpdate",
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'go',
+          'gomod',
+          'lua',
+          'ruby',
+          'vimdoc',
+          'vim',
+          'bash',
+          'fish',
+          'markdown',
+          'markdown_inline',
+          'mermaid'
+        },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<space>", -- maps in normal mode to init the node/scope selection with space
+            node_incremental = "<space>", -- increment to the upper named parent
+            node_decremental = "<bs>", -- decrement to the previous node
+            scope_incremental = "<tab>", -- increment to the upper scope (as defined in locals.scm)
+          },
+        },
+        autopairs = {
+          enable = true,
+        },
+        highlight = {
+          enable = true,
+
+          -- Disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              ["iB"] = "@block.inner",
+              ["aB"] = "@block.outer",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']]'] = '@function.outer',
+            },
+            goto_next_end = {
+              [']['] = '@function.outer',
+            },
+            goto_previous_start = {
+              ['[['] = '@function.outer',
+            },
+            goto_previous_end = {
+              ['[]'] = '@function.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>sn'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>sp'] = '@parameter.inner',
+            },
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    config = function() 
+      require("nvim-autopairs").setup {
+        check_ts = true,
+      }
+    end
+  },
+
+})
+
+----------------
+--- SETTINGS ---
+----------------
+
+-- disable netrw at the very start of our init.lua, because we use nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true -- Enable 24-bit RGB colors
+
+vim.opt.number = true        -- Show line numbers
+vim.opt.showmatch = true     -- Highlight matching parenthesis
+vim.opt.splitright = true    -- Split windows right to the current windows
+vim.opt.splitbelow = true    -- Split windows below to the current windows
+vim.opt.autowrite = true     -- Automatically save before :next, :make etc.
+vim.opt.autochdir = true     -- Change CWD when I open a file
+
+vim.opt.mouse = 'a'                -- Enable mouse support
+vim.opt.clipboard = 'unnamedplus'  -- Copy/paste to system clipboard
+vim.opt.swapfile = false           -- Don't use swapfile
+vim.opt.ignorecase = true          -- Search case insensitive...
+vim.opt.smartcase = true           -- ... but not it begins with upper case 
+vim.opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
+
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "undo"
+
+-- Indent Settings
+-- I'm in the Spaces camp (sorry Tabs folks), so I'm using a combination of
+-- settings to insert spaces all the time. 
+vim.opt.expandtab = true  -- expand tabs into spaces
+vim.opt.shiftwidth = 2    -- number of spaces to use for each step of indent.
+vim.opt.tabstop = 2       -- number of spaces a TAB counts for
+vim.opt.autoindent = true -- copy indent from current line when starting a new line
+vim.opt.wrap = true
+
+-- This comes first, because we have mappings that depend on leader
+-- With a map leader it's possible to do extra key combinations
+-- i.e: <leader>w saves the current file
+vim.g.mapleader = ','
+
+-- Fast saving
+vim.keymap.set('n', '<Leader>w', ':write!<CR>')
+vim.keymap.set('n', '<Leader>q', ':q!<CR>', { silent = true })
+
+-- Some useful quickfix shortcuts for quickfix
+vim.keymap.set('n', '<C-n>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-m>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>a', '<cmd>cclose<CR>')
+
+-- Remove search highlight
+vim.keymap.set('n', '<Leader><space>', ':nohlsearch<CR>')
+
+-- Search mappings: These will make it so that going to the next one in a
+-- search will center on the line it's found in.
+vim.keymap.set('n', 'n', 'nzzzv', {noremap = true})
+vim.keymap.set('n', 'N', 'Nzzzv', {noremap = true})
+
+-- Don't jump forward if I higlight and search for a word
+local function stay_star()
+  local sview = vim.fn.winsaveview()
+  local args = string.format("keepjumps keeppatterns execute %q", "sil normal! *")
+  vim.api.nvim_command(args)
+  vim.fn.winrestview(sview)
+end
+vim.keymap.set('n', '*', stay_star, {noremap = true, silent = true})
+
+-- We don't need this keymap, but here we are. If I do a ctrl-v and select
+-- lines vertically, insert stuff, they get lost for all lines if we use
+-- ctrl-c, but not if we use ESC. So just let's assume Ctrl-c is ESC.
+vim.keymap.set('i', '<C-c>', '<ESC>')
+
+-- If I visually select words and paste from clipboard, don't replace my
+-- clipboard with the selected word, instead keep my old word in the
+-- clipboard
+vim.keymap.set("x", "p", "\"_dP")
+
+-- rename the word under the cursor 
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Better split switching
+vim.keymap.set('', '<C-j>', '<C-W>j')
+vim.keymap.set('', '<C-k>', '<C-W>k')
+vim.keymap.set('', '<C-h>', '<C-W>h')
+vim.keymap.set('', '<C-l>', '<C-W>l')
+
+-- Visual linewise up and down by default (and use gj gk to go quicker)
+vim.keymap.set('n', '<Up>', 'gk')
+vim.keymap.set('n', '<Down>', 'gj')
+
+
+-- Terminal
+-- Clost terminal window, even if we are in insert mode
+vim.keymap.set('t', '<leader>q', '<C-\\><C-n>:q<cr>')
+
+-- switch to normal mode with esc
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>')
+
+-- Open terminal in vertical and horizontal split
+vim.keymap.set('n', '<leader>tv', '<cmd>vnew term://fish<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>ts', '<cmd>split term://fish<CR>', { noremap = true })
+
+-- Open terminal in vertical and horizontal split, inside the terminal
+vim.keymap.set('t', '<leader>tv', '<c-w><cmd>vnew term://fish<CR>', { noremap = true })
+vim.keymap.set('t', '<leader>ts', '<c-w><cmd>split term://fish<CR>', { noremap = true })
+
+-- mappings to move out from terminal to other views
+vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h')
+vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j')
+vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k')
+vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l')
+
+-- Open help window in a vertical split to the right.
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = vim.api.nvim_create_augroup("help_window_right", {}),
+    pattern = { "*.txt" },
+    callback = function()
+        if vim.o.filetype == 'help' then vim.cmd.wincmd("L") end
+    end
+})
+
+-- don't show number
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = [[setlocal nonumber norelativenumber]]
+})
+
+
+-- git.nvim
+vim.keymap.set('n', '<leader>gb', '<CMD>lua require("git.blame").blame()<CR>')
+vim.keymap.set('n', '<leader>go', "<CMD>lua require('git.browse').open(false)<CR>")
+vim.keymap.set('x', '<leader>go', ":<C-u> lua require('git.browse').open(true)<CR>")
+
+vim.api.nvim_create_user_command("GBrowse", 'lua require("git.browse").open(true)<CR>', {
+  range = true,
+  bang = true,
+  nargs = "*",
+})
+
+-- File-tree mappings
+vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<leader>f', ':NvimTreeFindFile!<CR>', { noremap = true })
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-b>', builtin.find_files, {})
+vim.keymap.set('n', '<C-g>', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>td', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
+vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
+
+-- diagnostics
+vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>ds', vim.diagnostic.setqflist)
+
+-- disable diagnostics, I didn't like them
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function() end
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+
+    vim.keymap.set('n', 'gd', "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    vim.keymap.set('n', '<leader>v', "<cmd>vsplit | lua vim.lsp.buf.definition()<CR>", opts)
+    vim.keymap.set('n', '<leader>s', "<cmd>belowright split | lua vim.lsp.buf.definition()<CR>", opts)
+
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+  end,
+})
+
+vim.api.nvim_create_autocmd('VimLeave', {
+  group = exitgroup,
+  pattern = { '*' },
+  command = [[call chansend(v:stderr, "\033]7;\033\\")]],
+})
+
